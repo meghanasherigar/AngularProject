e53c685a746dc427fe053fcc1029c05a6ea070e7
Meghana <meghanasherigar8@gmail.com>
2018-05-10 23:27:28 +0530

    small error fix

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {LoginService} from '../login.service';
+import { LoginService } from '../login.service';
 import { User } from '../user';
 import { Router } from '@angular/router';
-import {Observable} from 'rxjs/Observable';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-home',
@@ -11,28 +11,30 @@ import {Observable} from 'rxjs/Observable';
 })
 
 export class HomeComponent implements OnInit {
-	user:User;
-	allUser:User[]=[];
-  constructor(private service:LoginService,private router: Router) { }
+  user: User;
+  loginUser: User;
+
+  allUser: User[] = [];
+  constructor(private service: LoginService, private router: Router) { }
 
   ngOnInit() {
-  	this.user=this.service.getData();
-  	this.service.setUserData(this.user);
-  	// console.log(this.user);
-  	this.service.getUsers().subscribe(data => {
+    this.user = this.service.getData();
+    this.service.setUserData(this.user);
+    console.log(this.user);
+    this.service.getUsers().subscribe(data => {
       this.allUser = data;
-    }
+    });
   }
 
-  showLoginUserDetails(){
-  	this.loginUser = this.service.getUserData();
-  	this.service.setData(this.loginUser);
-  	this.router.navigate(['user-detail']);    	 
+  showLoginUserDetails() {
+    this.loginUser = this.service.getUserData();
+    this.service.setData(this.loginUser);
+    this.router.navigate(['user-detail']);
   }
 
-  showUsersDetails(user){
-  	this.service.setData(user);
-  	this.router.navigate(['user-detail']);
+  showUsersDetails(user) {
+    this.service.setData(user);
+    this.router.navigate(['user-detail']);
   }
 
 }
